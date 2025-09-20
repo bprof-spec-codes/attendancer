84c2a8b87b2103f8b5d2c2841cfaa74a9b29eb53
SefiTamas <schaffertamas@stud.uni-obuda.hu>
2025-09-20 12:22:43 +0200

    add migration with example data

---
+++ attendancer/backend/AttenDancer/Migrations/20250920102127_CreateDbWithExampleData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AttenDancer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDbWithExampleData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    JobTitle = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Age", "FullName", "JobTitle" },
+                values: new object[,]
+                {
+                    { 1, 47, "Kai Le", "Controls Engineer" },
+                    { 2, 38, "Robert Patel", "Analyst" },
+                    { 3, 52, "Cameron Lo", "Network Administrator" },
+                    { 4, 44, "Harper Castillo", "Director" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
