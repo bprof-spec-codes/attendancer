b37eb961ddcd80c5e7c87fd251bd2cd560e08202
SefiTamas <schaffertamas@stud.uni-obuda.hu>
2025-09-20 12:07:35 +0200

    add base entity

---
+++ attendancer/backend/AttenDancer/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AttenDancer.Entities;
+
+public class Entity : IEntity
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public int Id { get; set; }
+}
---
+++ attendancer/backend/AttenDancer/Entities/IEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace AttenDancer.Entities;
+
+public interface IEntity
+{
+    int Id { get; set; }
+}
