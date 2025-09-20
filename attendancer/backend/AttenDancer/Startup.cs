7fbeeefe969643a32d3b08f749e191bf7bc0bdb1
SefiTamas <schaffertamas@stud.uni-obuda.hu>
2025-09-20 12:05:03 +0200

    init Startup.cs

---
+++ attendancer/backend/AttenDancer/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace AttenDancer;
+
+public class Startup(IConfiguration configuration)
+{
+    public IConfiguration Configuration { get; } = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            // Swagger
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
