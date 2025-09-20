fb7fe1392451ed47cc24c00b669045f1055b56c9
SefiTamas <schaffertamas@stud.uni-obuda.hu>
2025-09-20 12:14:04 +0200

    add example entity

---
+++ attendancer/backend/AttenDancer/Entities/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace AttenDancer.Entities;
+
+public class Employee : Entity
+{
+    public string FullName { get; set; }
+    public int Age { get; set; }
+    public string JobTitle { get; set; }
+}
