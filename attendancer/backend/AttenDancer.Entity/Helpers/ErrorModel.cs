b34688ef55756473b76cb76cca7685089245f398
Kelemen Özséb <kelemenozzseb@gmail.com>
2025-10-05 18:35:40 +0200

    errormodel added

---
+++ attendancer/backend/AttenDancer.Entity/Helpers/ErrorModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttenDancer.Entity.Helpers
+{
+    public class ErrorModel
+    {
+        public string ErrorMessage { get; set; }
+        public ErrorModel(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+    }
+}
