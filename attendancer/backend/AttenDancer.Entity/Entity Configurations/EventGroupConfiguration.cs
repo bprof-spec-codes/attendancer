36732e45f2288430f7f341b5cadade455098ca39
Kelemen Özséb <kelemenozzseb@gmail.com>
2025-10-09 19:58:19 +0200

    BE entity configurations refact

---
+++ attendancer/backend/AttenDancer.Entity/Entity Configurations/EventGroupConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AttenDancer.Entity.Entity_Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AttenDancer.Entity.Entity_Configurations
+{
+    internal class EventGroupConfiguration : IEntityTypeConfiguration<EventGroup>
+    {
+        public void Configure(EntityTypeBuilder<EventGroup> builder)
+        {
+            builder.Property(eg => eg.Name).HasMaxLength(100);
+
+            builder.HasOne(eg => eg.User)
+                .WithMany(u => u.EventGroups)
+                .HasForeignKey(eg => eg.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
+
