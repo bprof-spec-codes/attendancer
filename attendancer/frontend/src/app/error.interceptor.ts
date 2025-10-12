2fe4231a32a2d709b0acc4e5207d96b6fa669a39
SefiTamas <schaffertamas@stud.uni-obuda.hu>
2025-10-12 13:37:31 +0200

    add error interceptor

---
+++ attendancer/frontend/src/app/error.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core'
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from '@angular/common/http'
+import { catchError, Observable, throwError } from 'rxjs'
+
+const getErrorMsgByStatus = (status: number): string => {
+  switch (status) {
+    case 0:
+      return 'Nem sikerült csatlakozni a szerverhez. Ellenőrizd az internetkapcsolatot.'
+    case 401:
+      return 'A művelethez be kell jelentkezni.'
+    case 403:
+      return 'Nincs jogosultságod a művelethez.'
+    case 404:
+      return 'A kért erőforrás nem található.'
+    case 500:
+      return 'Szerverhiba történt, próbáld újra később.'
+    default:
+      return 'Ismeretlen hiba történt.'
+  }
+}
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        const msg = getErrorMsgByStatus(error.status)
+        return throwError(() => error)
+      })
+    )
+  }
+}
