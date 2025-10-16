aca716014bbc0ae07d1e4dfc828a009e8036f133
rezsnyakdani <rezsnyakd@gmail.com>
2025-10-16 23:15:31 +0200

    AuthService done

--- attendancer/backend/AttenDancer.Logic/Services/AuthService.cs
+++ attendancer/backend/AttenDancer.Logic/Services/AuthService.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AttenDancer.Entity.Entity_Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AttenDancer.Logic.Services
 {
-    internal class AuthService
+    public class AuthService
     {
+        private readonly IConfiguration configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateJwtToken(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
