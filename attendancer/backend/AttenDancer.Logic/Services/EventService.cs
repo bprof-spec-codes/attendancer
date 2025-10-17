4339eaab4bf6d21039b59c25194e281c6dbafc9e
Kelemen Özséb <kelemenozzseb@gmail.com>
2025-10-17 21:16:52 +0200

    event create done

---
+++ attendancer/backend/AttenDancer.Logic/Services/EventService.cs
@@ -0,0 +1,29 @@
+﻿using AttenDancer.Data.Repositories;
+using AttenDancer.Entity.Dtos.Event;
+using AttenDancer.Entity.Entity_Models;
+using AttenDancer.Logic.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttenDancer.Logic.Services
+{
+    public class EventService
+    {
+        private readonly IRepository<Event> _eventRepository;
+        DtoProvider dtoProvider;
+        public EventService(IRepository<Event> eventRepository, DtoProvider dtoProvider)
+        {
+            _eventRepository = eventRepository;
+            this.dtoProvider = dtoProvider;
+        }
+
+        public async Task<Event> CreateEventAsync(EventCreateDto createDto)
+        {
+            var newEvent = dtoProvider.Mapper.Map<Event>(createDto);
+            return await _eventRepository.Create(newEvent);
+        }
+    }
+}
