8367a1d86313ae2a1822a1d8be6ed50a38ca85f4
Kelemen Özséb <kelemenozzseb@gmail.com>
2025-10-17 21:20:22 +0200

    event get methods done

--- attendancer/backend/AttenDancer.Logic/Services/EventService.cs
+++ attendancer/backend/AttenDancer.Logic/Services/EventService.cs
@@ -2,6 +2,7 @@
 using AttenDancer.Entity.Dtos.Event;
 using AttenDancer.Entity.Entity_Models;
 using AttenDancer.Logic.Helper;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,38 @@ namespace AttenDancer.Logic.Services
         {
             var newEvent = dtoProvider.Mapper.Map<Event>(createDto);
             return await _eventRepository.Create(newEvent);
+        }
+
+        public async Task<EventViewDto> GetEvent(string eventId)
+        {
+            Event? getevent = await _eventRepository.GetAll().FirstOrDefaultAsync(e => e.Id == eventId);
+
+            if (getevent == null)
+            {
+                throw new Exception("Hibás esemény azonosító");
+            }
+
+            EventViewDto eventView = dtoProvider.Mapper.Map<EventViewDto>(getevent);
+            return eventView;
+        }
+
+        public async Task<List<EventViewDto>> GetAllEvents()
+        {
+            List<EventViewDto> events = dtoProvider.Mapper.Map<List<EventViewDto>>(await _eventRepository.GetAll().ToListAsync());
+            return events;
+        }
+
+        public async Task<EventViewDto> GetEventByUserId(string userId)
+        {
+            Event? getevent = await _eventRepository.GetAll().FirstOrDefaultAsync(e => e.Participants.Any(u => u.UserId == userId));
+
+            if (getevent == null)
+            {
+                throw new Exception("Hibás esemény azonosító");
+            }
+
+            EventViewDto eventView = dtoProvider.Mapper.Map<EventViewDto>(getevent);
+            return eventView;
         }
     }
 }
