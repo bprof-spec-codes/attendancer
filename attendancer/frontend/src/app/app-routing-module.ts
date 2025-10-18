838078c0a6c80718b5d254e66fde9e8157d5f136
NagyBendeguz <bendeguz@medea.hu>
2025-10-18 19:03:41 +0200

    modify sheet by using mock data

--- attendancer/frontend/src/app/app-routing-module.ts
+++ attendancer/frontend/src/app/app-routing-module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Registration } from './registration/registration';
+import { Sheet } from './sheet/sheet';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: Login },
   { path: 'register', component: Registration },
+  { path: 'sheet', component: Sheet },
   { path: '**', redirectTo: 'login' },
 ];
 
--- attendancer/frontend/src/app/sheet/sheet.ts
+++ attendancer/frontend/src/app/sheet/sheet.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MockDataService } from '../services/mock-data.service';
 
 @Component({
   selector: 'app-sheet',
@@ -7,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './sheet.sass'
 })
 export class Sheet {
+  event: any
+  participants: any
+  presentCount: number = 0
+  absentCount: number = 0
 
+  constructor(private mockDataService: MockDataService) {}
+
+  ngOnInit(): void {
+    this.mockDataService.getEventById().subscribe((data) => {
+      this.event = data;
+    });
+    this.mockDataService.getParticipantsByEventId().subscribe((data) => {
+      this.participants = data;
+    });
+    this.countPresent()
+  }
+
+  countPresent(): void {
+    for (let i = 0; i < this.participants.length; i++) {
+      if (this.participants[i].present) {
+        this.presentCount++
+      }
+    }
+
+    this.absentCount = this.participants.length - this.presentCount
+  }
 }
