be2c17b9ecf74797db16bcc8eafcab6b16a32184
OrcsikRoland <orcsikroland21@gmail.com>
2025-10-20 15:22:02 +0200

    feat: initialize environment configuration with API endpoints and token key

--- attendancer/frontend/src/environments/environment.development.ts
+++ attendancer/frontend/src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  apis: {
+    login: "https://localhost:5001/api/auth/login",
+    logout: "https://localhost:5001/api/auth/logout",
+    register: "https://localhost:5001/api/auth/register"
+  },
+  tokenKey: "attendancer-jwt-token"
+};
