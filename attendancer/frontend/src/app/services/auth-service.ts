ceb9dbee8c2391a48afdbcdf88872fc6bf49c691
OrcsikRoland <orcsikroland21@gmail.com>
2025-10-20 13:10:27 +0200

    refactor: clean up AuthService methods and ensure proper token handling in login

--- attendancer/frontend/src/app/services/auth-service.ts
+++ attendancer/frontend/src/app/services/auth-service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { RegisterModel } from '../models/register-model';
 import { environment } from '../../environments/environment';
+import { LoginModel } from '../models/login-model';
 
 
 @Injectable({
@@ -13,22 +14,40 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   register(registerModel: RegisterModel): void {
-      this.http.post<{ token: string }>(environment.apis.register, registerModel)
-        .subscribe({
-          next: (res) => {
-            const token = res?.token;
-            if (!token) {
-              console.error('Register response does not contain a token.');
-              return;
-            }
-            localStorage.setItem(environment.tokenKey, token);
-            console.log('Registration successful!');
+    this.http.post<{ token: string }>(environment.apis.register, registerModel)
+      .subscribe({
+        next: (res) => {
+          const token = res?.token;
+          if (!token) {
+            console.error('Register response does not contain a token.');
+            return;
+          }
+          localStorage.setItem(environment.tokenKey, token);
+          console.log('Registration successful!');
           },
           error: (err) => {
             console.error('Registration error:', err);
           }
-        });
-    }
+      });
+  }
+
+  login(loginModel: LoginModel): void {
+    this.http.post<{ token: string}>(environment.apis.login, loginModel)
+    .subscribe({
+      next: (res) => {
+        const token = res?.token
+        if (!token) {
+          console.error('Login response does not contain a token.')
+          return
+        }
+        localStorage.setItem(environment.tokenKey, token)
+        console.log('Registration successful!')
+      },
+      error: (err) => {
+        console.error('Login error:', err)
+      }
+    })
+  }
 
 
 }
