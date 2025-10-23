9836b936607e652808c1392ad6192fd2e50e9b31
OrcsikRoland <orcsikroland21@gmail.com>
2025-10-23 19:29:59 +0200

    feat: update API endpoints in environment files and refactor registration and login components for improved error handling and validation

--- attendancer/frontend/src/environments/environment.development.ts
+++ attendancer/frontend/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   apis: {
-    login: "https://localhost:5001/api/auth/login",
-    logout: "https://localhost:5001/api/auth/logout",
-    register: "https://localhost:5001/api/auth/register"
+    login: "https://localhost:7198/api/User/login",
+    logout:   "https://localhost:7198/api/User/logout",
+    register: "https://localhost:7198/api/User/register"
   },
   tokenKey: "attendancer-jwt-token"
 };
