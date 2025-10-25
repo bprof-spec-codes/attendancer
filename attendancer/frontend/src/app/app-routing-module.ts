be5d5c9a56b365e3c3fba51987d7b75207384d23
Adam Revesz <revike6@gmail.com>
2025-10-25 22:44:30 +0200

    Installed Bootstrap, Made the profile component with bootstrap. Other components will use that too

--- attendancer/frontend/src/app/app-routing-module.ts
+++ attendancer/frontend/src/app/app-routing-module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Registration } from './registration/registration';
 import { SheetForm } from './sheet-form/sheet-form';
+import { Profile } from './profile/profile';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: Login },
   { path: 'register', component: Registration },
-  {path: 'createSheet', component: SheetForm},
+  { path: 'createSheet', component: SheetForm },
+  { path: 'profile', component: Profile },
   { path: '**', redirectTo: 'login' },
 ];
 
