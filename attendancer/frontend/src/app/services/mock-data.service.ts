a68e44d69ebcf5a8125d262cd9ddd9ee22644eef
NagyBendeguz <bendeguz@medea.hu>
2025-11-01 18:46:32 +0100

    add mock data for create sheet

--- attendancer/frontend/src/app/services/mock-data.service.ts
+++ attendancer/frontend/src/app/services/mock-data.service.ts
@@ -9,9 +9,10 @@ export class MockDataService {
 
   // Egy esemény adatainak lekérdezése az id-je alapján.
   // A mock-ban már a id alapján visszaadott értéket szimulálja.
-  // /api/Event/{eventid}
+  // GET /api/Event/{eventid}
   getEventById(eventId: string): Observable<any> {
     let mockData: any = {}
+
     if (eventId === "1") {
       mockData =
       {
@@ -47,9 +48,10 @@ export class MockDataService {
 
   // Egy esemény résztvevőinek lekérdezése.
   // A mock-ban már az esemény id alapján visszaadott résztvevők adatait szimulálja.
-  // /api/Participant/{eventid}
+  // GET /api/Participant/{eventid}
   getParticipantsByEventId(eventId: string): Observable<any> {
     let mockData: any[] = []
+
     if (eventId === "1") {
       mockData = 
       [
@@ -167,5 +169,168 @@ export class MockDataService {
     }
 
     return of(mockData);
+  }
+
+  // Egy jelenleg bejelentkezett felhasználó által készített esemény csoportok neveinek és metaadatának lekérdezése.
+  // A mock-ban a bejelentkezett felhasználó az id-je alapján a saját maga készített esemény csoportok neveit és metaadatait kapja vissza.
+  // GET - /api/EventGroup/{userid}
+  getEventGroupsWithMetadataByUserId(userId: string): Observable<any> {
+    let mockData: any[] = []
+
+    if (userId === "1") {
+      mockData = [
+        {
+          "id": "sdfga32sfd",
+          "name": "Angular",
+          "metadata": [
+            "Neptun code",
+            "Email",
+            "Phone number"
+          ]
+        },
+        {
+          "id": "sdgfsd2qq",
+          "name": "Mobil Prog",
+          "metadata": [
+            "Neptun code",
+            "Seat"
+          ]
+        },
+        {
+          "id": "jmhhgf34",
+          "name": "DevOps",
+          "metadata": [
+            "Neptun code",
+            "Email"
+          ]
+        }
+      ]
+    }
+    else if (userId === "2") {
+      mockData = [
+        {
+          "id": "d8fgzd",
+          "name": ".NET",
+          "metadata": [
+            "Neptun code",
+            "Email",
+            "Phone number"
+          ]
+        },
+        {
+          "id": "ghferf43u8u7",
+          "name": "Beágyazott rendszerek",
+          "metadata": [
+            "Neptun code",
+            "Seat"
+          ]
+        },
+        {
+          "id": "asgf43jhed",
+          "name": "Rendszerelmélet",
+          "metadata": [
+            "Neptun code",
+            "Email"
+          ]
+        }
+      ]
+    }
+    else {
+      mockData = [
+        {
+          "id": "rfgsdfdrt542gd2",
+          "name": "DIMAT",
+          "metadata": [
+            "Neptun code",
+            "Email",
+            "Phone number"
+          ]
+        },
+        {
+          "id": "asfd54vd34",
+          "name": "Analízis",
+          "metadata": [
+            "Neptun code",
+            "Seat"
+          ]
+        },
+        {
+          "id": "xcv52jhsad34fd",
+          "name": "Hálózatok",
+          "metadata": [
+            "Neptun code",
+            "Email"
+          ]
+        }
+      ]
+    }
+
+    return of(mockData)
+  }
+
+  // Egy jelenleg bejelentkezett felhasználó által készített események lekérdezése.
+  // A mock-ban a bejelentkezett felhasználó az id-je alapján a saját maga készített eseményeket kapja vissza.
+  // GET - /api/Event/{userid}
+  getEventsByUserId(userId: string): Observable<any> {
+    let mockData: any[] = []
+
+    if (userId === "1") {
+      mockData = [
+        {
+          "id": "gsdaafr3eqw",
+          "name": "Űrhajós előadás",
+          "eventGroupId": "",
+          "metadata": [
+            "Neptun code",
+            "Email",
+            "Phone number"
+          ]
+        },
+        {
+          "id": "scadv43dnes543dv",
+          "name": "XYZ Megbeszélés",
+          "eventGroupId": "",
+          "metadata": [
+            "Seat",
+            "Email"
+          ]
+        }
+      ]
+    }
+    else if (userId === "2") {
+      mockData = [
+        {
+          "id": "fsd4535fdgs54wdsa",
+          "name": "ZYX Megbeszélés",
+          "eventGroupId": "",
+          "metadata": [
+            "Seat",
+            "Phone number",
+            "Email"
+          ]
+        }
+      ]
+    }
+    else {
+      mockData = mockData = [
+        {
+          "id": "asrtevdf3w456zthfdds",
+          "name": "Egyszeri esemény",
+          "eventGroupId": "",
+          "metadata": [
+            "Seat"
+          ]
+        }
+      ]
+    }
+
+    return of(mockData)
+  }
+
+  // Egy új esemény hozzáadása.
+  // POST - /api/Event
+  postEvent(event: any) {
+    console.log("The following data will be sent to the backend:")
+    console.log(event)
   }
 }
