d9e95bc6e02733b39ac80e5477c466ff558a718d
Benedek5@stud.uni-obuda.hu <benedek.szabo@stud.uni-obuda.hu>
2025-11-07 18:22:28 +0100

    Add User Crud methods to UserService

--- attendancer/backend/AttenDancer.Logic/Services/UserService.cs
+++ attendancer/backend/AttenDancer.Logic/Services/UserService.cs
@@ -1,5 +1,7 @@
 ﻿using AttenDancer.Data.Repositories;
+using AttenDancer.Entity.Dtos.User;
 using AttenDancer.Entity.Entity_Models;
+using AttenDancer.Logic.Helper;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -14,18 +16,19 @@ namespace AttenDancer.Logic.Services
 
         private readonly IRepository<User> _userRepository;
         private readonly AuthService _authService;
+        private readonly DtoProvider _dtoProvider;
 
-        public UserService(IRepository<User> userRepository, AuthService authService)
+        public UserService(IRepository<User> userRepository, AuthService authService, DtoProvider dtoProvider)
         {
             _userRepository = userRepository;
             _authService = authService;
+            _dtoProvider = dtoProvider;
         }
 
-        //loginban meg kell majd hívni az _authService.GenerateJwtToken(user)-t
 
         public async Task<User> RegisterAsync(string firstName, string lastName, string email, string password)
         {
-            
+
             var exists = await _userRepository.GetAll()
                 .AnyAsync(u => u.Email == email.ToLower());
 
@@ -34,10 +37,10 @@ namespace AttenDancer.Logic.Services
                 throw new Exception("Ez az email cím már regisztrálva van");
             }
 
-            
+
             string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
 
-            
+
             var user = new User
             {
                 FirstName = firstName,
@@ -68,5 +71,88 @@ namespace AttenDancer.Logic.Services
 
             return user;
         }
+
+
+        public async Task<UserResponseDto> GetByIdAsync(string id)
+        {
+            var user = await _userRepository.GetOne(id);
+            if (user == null)
+            {
+                throw new Exception("Felhasználó nem található");
+            }
+            return _dtoProvider.Mapper.Map<UserResponseDto>(user);
+        }
+
+        public async Task<List<UserResponseDto>> GetAllUsersAsync()
+        {
+            var users = await _userRepository.GetAll().ToListAsync();
+            return _dtoProvider.Mapper.Map<List<UserResponseDto>>(users);
+        }
+
+
+
+
+        public async Task<UserResponseDto> UpdateAsync(string id, UserUpdateDto updateDto)
+        {
+            var user = await _userRepository.GetOne(id);
+            if (user == null)
+            {
+                throw new Exception("Felhasználó nem található");
+            }
+
+            if (user.Email != updateDto.Email.ToLower())
+            {
+                var emailExists = await _userRepository.GetAll()
+                    .AnyAsync(u => u.Email == updateDto.Email.ToLower() && u.Id != id);
+                if (emailExists)
+                {
+                    throw new Exception("Ez az email cím már használatban van");
+                }
+            }
+
+            user.FirstName = updateDto.FirstName;
+            user.LastName = updateDto.LastName;
+            user.Email = updateDto.Email.ToLower();
+
+            var updatedUser = await _userRepository.Update(user);
+            return _dtoProvider.Mapper.Map<UserResponseDto>(updatedUser);
+        }
+
+
+        public async Task ChangePasswordAsync(string id, UserChangePassword changePasswordDto)
+        {
+            var user = await _userRepository.GetOne(id);
+            if (user == null)
+            {
+                throw new Exception("Felhasználó nem található");
+            }
+
+            bool isOldPasswordValid = BCrypt.Net.BCrypt.Verify(changePasswordDto.OldPassword, user.Password);
+            if (!isOldPasswordValid)
+            {
+                throw new Exception("A jelenlegi jelszó helytelen");
+            }
+
+            user.Password = BCrypt.Net.BCrypt.HashPassword(changePasswordDto.NewPassword);
+
+            await _userRepository.Update(user);
+
+        }
+
+
+        public async Task DeleteAsync(string id)
+        {
+            var user = await _userRepository.GetOne(id);
+            if (user == null)
+            {
+                throw new Exception("Felhasználó nem található");
+            }
+            await _userRepository.DeleteById(id);
+
+
+
+
+        }
+
     }
 }
