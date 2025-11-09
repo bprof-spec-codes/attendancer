c6c3afbf3b374cb4ee00b5a71567a6bef06a31b0
OrcsikRoland <orcsikroland21@gmail.com>
2025-11-09 16:45:42 +0100

    remove logout functionality from navigation component

--- attendancer/frontend/src/app/nav/nav.ts
+++ attendancer/frontend/src/app/nav/nav.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../services/auth-service';
 
 @Component({
   selector: 'app-nav',
@@ -8,9 +7,5 @@ import { AuthService } from '../services/auth-service';
   styleUrl: './nav.sass'
 })
 export class Nav {
-  constructor(private authService: AuthService) {}
 
-  logout() {
-    this.authService.logout()
-  }
 }
