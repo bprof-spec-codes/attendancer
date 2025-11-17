99371bfc04fa182c4889fadacb67186e11d6b557
rezsnyakdani <rezsnyakd@gmail.com>
2025-11-17 10:25:35 +0100

    fix event in database, add 20251117080121_AddExpirationDateToEvent migration

---
+++ attendancer/backend/AttenDancer.Data/Migrations/20251117080121_AddExpirationDateToEvent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttenDancer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExpirationDateToEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpirationDate",
+                table: "Events",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "DATEADD(day, 1, GETUTCDATE())"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpirationDate",
+                table: "Events"
+            );
+        }
+    }
+}
