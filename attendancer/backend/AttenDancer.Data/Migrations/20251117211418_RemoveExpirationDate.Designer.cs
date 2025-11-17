99955dead568c196e31ddc939f3cb5df0eb3cb6c
rezsnyakdani <rezsnyakd@gmail.com>
2025-11-17 22:17:06 +0100

    Add migration RemoveExpirationDate

--- attendancer/backend/AttenDancer.Data/Migrations/20251117211418_RemoveExpirationDate.Designer.cs
+++ attendancer/backend/AttenDancer.Data/Migrations/20251117211418_RemoveExpirationDate.Designer.cs
@@ -4,6 +4,7 @@ using AttenDancer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AttenDancer.Data.Migrations
 {
     [DbContext(typeof(AttenDancerDbContext))]
-    partial class AttenDancerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251117211418_RemoveExpirationDate")]
+    partial class RemoveExpirationDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
