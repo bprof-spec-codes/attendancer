c15ae94ae33c46d53ac2ecb185d2807ed5237e93
Adam Revesz <revike6@gmail.com>
2025-11-19 13:31:27 +0100

    Created a mobile view for the profile component and fixed a few issues with responsivity in other components

--- attendancer/frontend/src/app/profile/profile.ts
+++ attendancer/frontend/src/app/profile/profile.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MockDataService } from '../services/mock-data.service';
 
 @Component({
@@ -16,6 +16,20 @@ export class Profile implements OnInit {
       "signedEvents": []
     }
   ]
+  isMobile = false;
+
+  private updateIsMobile() {
+    if (typeof window !== 'undefined' && window.matchMedia) {
+      this.isMobile = window.matchMedia('(max-width: 1100px)').matches;
+    }
+  }
+
+  @HostListener('window:resize')
+  @HostListener('window:orientationchange')
+  onResize() {
+    this.updateIsMobile();
+  }
+
   user: any = { // egy User mock-olása - itt majd ez lesz: user: User
     lastName: "",
     firstName: "",
@@ -23,7 +37,7 @@ export class Profile implements OnInit {
   }
   userId: string = (Math.floor(Math.random() * 3) + 1).toString() // a bejelentkezett felhasználó id-jének mock-olása
 
-  constructor(private mockDataService: MockDataService) {}
+  constructor(private mockDataService: MockDataService) { }
 
   ngOnInit(): void {
     this.mockDataService.getUserByUserId(this.userId).subscribe((data) => {
