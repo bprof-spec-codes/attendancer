cce3b4d0f620a6ef30bcf63b57599b6712981a72
rezsnyakdani <rezsnyakd@gmail.com>
2025-11-23 09:11:15 +0100

    Add EventGroupController.cs

---
+++ attendancer/backend/AttenDancer/Controllers/EventGroupController.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using AttenDancer.Entity.Dtos.EventGroup;
+using AttenDancer.Entity.Entity_Models;
+using AttenDancer.Logic.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttenDancer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventGroupController : ControllerBase
+    {
+        private readonly EventGroupService _eventGroupService;
+
+        public EventGroupController(EventGroupService eventGroupService)
+        {
+            _eventGroupService = eventGroupService;
+        }
+
+        
+    }
+}
