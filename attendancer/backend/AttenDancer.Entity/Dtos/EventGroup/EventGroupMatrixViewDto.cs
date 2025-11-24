be71428bf3de05a11b7b66ec93054204bae7377a
Benedek5@stud.uni-obuda.hu <benedek.szabo@stud.uni-obuda.hu>
2025-11-24 11:37:54 +0100

    Add EventGroupMatrixViewDto for matrix view

---
+++ attendancer/backend/AttenDancer.Entity/Dtos/EventGroup/EventGroupMatrixViewDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttenDancer.Entity.Dtos.EventGroup
+{
+    public class EventGroupMatrixViewDto
+    {
+        public string EventGroupId { get; set; } = null!;
+        public string EventGroupName { get; set; } = null!;
+        public List<MatrixEventColumnDto> Events { get; set; } = new();
+        public List<MatrixParticipantRowDto> Participants { get; set; } = new();
+
+    }
+
+    public class MatrixEventColumnDto 
+    {
+        public string EventId { get; set; } = null!;
+        public string EventName { get; set; } = null!;
+    }
+
+    public class MatrixParticipantRowDto
+    {
+        public string UserId { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string UserEmail { get; set; } = null!;
+        public Dictionary<string, bool> Attendances { get; set; } = new();
+    }
+}
