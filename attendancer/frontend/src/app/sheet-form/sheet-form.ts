47f0d6ef3c516461c120e9c11fccba2578e225e5
NagyBendeguz <bendeguz@medea.hu>
2025-11-25 16:08:35 +0100

    start using real data for create and edit sheet

--- attendancer/frontend/src/app/sheet-form/sheet-form.ts
+++ attendancer/frontend/src/app/sheet-form/sheet-form.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MockDataService } from '../services/mock-data.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EditEventService } from '../services/edit-event-service';
+import { EventClient, EventCreateDto } from '../app.api-client.generated';
+import { EventViewDto } from '../models/event-view-dto';
+import { JwtDecodeService } from '../services/jwt-decode.service';
 
 @Component({
   selector: 'app-sheet-form',
@@ -11,51 +14,70 @@ import { EditEventService } from '../services/edit-event-service';
 })
 export class SheetForm implements OnInit {
   customFields: number[] = []
-  selectedEventOrEventGroup: string = ""
-  currentEvent: any = {
-    id: "",
-    name: "",
-    date: "",
-    eventGroupId: "",
-    metadata: []
-  }
-  events: any[] = []
+  selectedEventOrEventGroup: string | undefined = ""
+  currentEvent: EventCreateDto = new EventCreateDto()
+  events: EventViewDto[] = []
   eventGroups: any[] = []
   currentlySelectedMetadata: string[] = []
-  userId: string = (Math.floor(Math.random() * 3) + 1).toString()
   editMode: boolean = false
+  public userId: string | null = null;
 
   constructor(
     private router: Router, 
     private route: ActivatedRoute, 
+    private eventClient: EventClient, 
     private mockDataService: MockDataService, 
-    private editEventService: EditEventService
+    private editEventService: EditEventService, 
+    private jwtDecodeService: JwtDecodeService
   ) {}
 
   ngOnInit(): void {
+    // A bejelentkezett felhasználó userId-jének megszerzése.
+    this.userId = this.jwtDecodeService.getUserId();
+    this.currentEvent.userId = this.userId ?? ""
+
     if (this.route.snapshot.routeConfig?.path === "editSheet") {
 
       this.editMode = true
 
       this.currentEvent = this.editEventService.getEvent();
 
+      // TODO TEMP
+      this.currentEvent.date = new Date("2025-11-25T10:00").toISOString().slice(0, 16)
+      // TODO TEMP
+
       if (this.currentEvent === undefined) {
         this.router.navigate(['/createSheet']);
       }
 
-      this.currentEvent.metadata.forEach(() => {
+      this.currentEvent.metadata!.forEach(() => {
         this.addCustomField()
       });
 
       this.selectedEventOrEventGroup = this.currentEvent.eventGroupId
-      this.onSelectionChange(this.currentEvent.eventGroupId)
+      this.onSelectionChange(this.currentEvent.eventGroupId!)
     }
-    
-    this.mockDataService.getEventsByUserId(this.userId).subscribe((data) => {
-      this.events = data;
+
+    // Leellenőrizni, hogy ne legyen undefined a metadata.
+    if (this.currentEvent.metadata === undefined) {
+      this.currentEvent.metadata = []
+    }
+
+    // Event -> Events ?
+    // Mindet visszaadja nem csak a csoport nélkülieket!
+    this.eventClient.getEventByUserId().subscribe((response) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const jsonData = JSON.parse(reader.result as string);
+        //console.log(jsonData)
+
+        this.events = jsonData;
+      };
+      reader.readAsText(response.data);
     });
 
-    this.mockDataService.getEventGroupsWithMetadataByUserId(this.userId).subscribe((data) => {
+    // TODO
+    this.mockDataService.getEventGroupsWithMetadataByUserId(this.userId!).subscribe((data) => {
       this.eventGroups = data;
     });
   }
@@ -81,39 +103,41 @@ export class SheetForm implements OnInit {
   createSheet() {
     if (!this.inputInvalid()) {
 
-      this.currentEvent.metadata = this.currentEvent.metadata.filter((data: undefined) => data !== undefined && data !== '')
+      this.currentEvent.metadata = this.currentEvent.metadata!.filter(data => data !== undefined && data !== '')
 
-      this.mockDataService.postEvent(this.currentEvent)
+      if (this.currentEvent.eventGroupId === undefined) {
+        this.currentEvent.eventGroupId = ""
+      }
+
+      //this.mockDataService.postEvent(this.currentEvent)
+      console.log(this.currentEvent)
+      this.eventClient.createEvent(this.currentEvent)
+
       this.resetPage()
     }
   }
 
-
   editSheet() {
     if (!this.inputInvalid()) {
 
-      this.currentEvent.metadata = this.currentEvent.metadata.filter((data: undefined) => data !== undefined && data !== '')
+      this.currentEvent.metadata = this.currentEvent.metadata!.filter(data => data !== undefined && data !== '')
 
       this.currentEvent.eventGroupId = this.selectedEventOrEventGroup
 
-      this.mockDataService.updateEvent(this.currentEvent)
+      //this.mockDataService.updateEvent(this.currentEvent)
+      this.eventClient.updateEvent(this.currentEvent.id!, this.currentEvent)
       this.router.navigate(['/profile']);
     }
   }
 
   inputInvalid(): boolean {
-    return !(this.currentEvent.name.length > 0 && this.currentEvent.date.length > 0)
+    return !(this.currentEvent.name && this.currentEvent.date)
   }
 
   resetPage(): void {
     this.customFields = []
     this.selectedEventOrEventGroup = ""
-    this.currentEvent = {
-      name: "",
-      eventGroupId: "",
-      date: "",
-      metadata: []
-    }
+    this.currentEvent = new EventCreateDto()
     this.events = []
     this.eventGroups = []
     this.currentlySelectedMetadata = []
@@ -127,6 +151,6 @@ export class SheetForm implements OnInit {
 
   removeCustomField(index: number): void {
     this.customFields.splice(index, 1);
-    this.currentEvent.metadata[index] = ""
+    this.currentEvent.metadata![index] = ""
   }
 }
