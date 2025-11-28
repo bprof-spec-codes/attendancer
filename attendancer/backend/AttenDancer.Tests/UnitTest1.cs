32d768f8b82f18151d69ba4cf6455647f6954392
Benedek5@stud.uni-obuda.hu <benedek.szabo@stud.uni-obuda.hu>
2025-11-28 14:40:31 +0100

    Add NUnit test project structure

---
+++ attendancer/backend/AttenDancer.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace AttenDancer.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
