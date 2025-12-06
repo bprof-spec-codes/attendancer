56739b760d8e4393d4d7bb398c077df22c4a6a04
NagyBendeguz <bendeguz@medea.hu>
2025-12-06 21:40:50 +0100

    fix folder names

--- attendancer/frontend/src/app/services/translation/translation.ts
+++ attendancer/frontend/src/app/services/translation/translation.ts
@@ -9,7 +9,7 @@ export class CustomTranslateLoader implements TranslateLoader {
   constructor(private http: HttpClient) {}
 
   getTranslation(lang: string): Observable<any> {
-    return this.http.get(`./assets/i18n/${lang}.json`);
+    return this.http.get(`./Assets/i18n/${lang}.json`);
   }
 }
 
